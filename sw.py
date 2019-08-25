c4719cf71562d8c2dad9f15df2c0d8e270b0f66c
ashkenn13 <44744037+ashkenn13@users.noreply.github.com>
2019-08-25 10:59:04 +0530

    Add files via upload

---
+++ sw.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+path=r"C:\\webdrivers\\chromedriver"
+driver = webdriver.Chrome(path)
+driver.get('http://web.whatsapp.com')
+
+n=int(input("Enter number of times : "))
+
+for i in range(1,n):
+    name = input('Enter the name of user or group : ')
+    msg = input('Enter the message : ')
+    count = int(input('Enter the count : '))
+
+    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+    user.click()
+
+    msg_box = driver.find_element_by_class_name('_13mgZ')
+
+    for i in range(count):
+        msg_box.send_keys(msg)
+        driver.find_element_by_class_name('_3M-N-').click()
